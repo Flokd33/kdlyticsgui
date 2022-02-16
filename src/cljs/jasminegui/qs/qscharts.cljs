6c3d5da366e178575d9736d7c83f95669d701d91
fcadet <florian.kd@gmail.com>
2022-02-16 12:01:38 +0000

    greenbondcalculator.cljs + historical graph

--- src/cljs/jasminegui/qs/qscharts.cljs
+++ src/cljs/jasminegui/qs/qscharts.cljs
@@ -12,6 +12,7 @@
     [jasminegui.tables :as tables]
     [jasminegui.static :as static]
     [jasminegui.tools :as t]
+    [cljs-time.core :as datecore]
     [reagent.core :as reagent]
     [reagent.core :as r]
     [oz.core :as oz]
@@ -20,8 +21,7 @@
     [reagent-contextmenu.menu :as rcm]
     [jasminegui.qs.qstables :as qstables]
     )
-;  (:import (java.time LocalDate)
-;)
+
   )
 
 (defn spot-chart-vega-spec [model ratings issuers rating-curves-key]
@@ -157,17 +157,14 @@
 (defn quant-isin-history-chart [price? ytw? ztw? duration? rating? isin1 isin2 ticker1 ticker2]
   (let [mapping (into {(keyword isin1) (str ticker1) (keyword isin2) (str ticker2)})
         data-pricing @(rf/subscribe [:quant-model/history-result])
-        data-to-plot (for [e data-pricing] (assoc e :ticker (mapping (keyword (e :ISIN)))))
+        data-to-plot (for [e data-pricing] (e :ticker (mapping (keyword (e :ISIN)))))
         start-date-yyyymmdd (t/gdate-to-yyyymmdd @(rf/subscribe [:quant-model/history-start-date]))
         start-date-yyyy-mm-dd (str (subs start-date-yyyymmdd 0 4) "-" (subs start-date-yyyymmdd 4 6) "-" (subs start-date-yyyymmdd 6 8))
-        ;end-date-yyyymmdd (t/gdate-to-yyyymmdd @(rf/subscribe [:quant-model/history-end-date]))
-        ;end-date-yyyy-mm-dd (str (subs end-date-yyyymmdd 0 4) "-" (subs end-date-yyyymmdd-yyyymmdd 4 6) "-" (subs end-date-yyyymmdd 6 8))
-        data-pricing-filtered (filter #(> (% :date) start-date-yyyy-mm-dd) data-pricing)
+        ;start-date-dateformat (datecore/date-time (cljs.reader/read-string (subs start-date-yyyymmdd 0 4)) (cljs.reader/read-string (subs start-date-yyyymmdd 4 6)) (cljs.reader/read-string (subs start-date-yyyymmdd 6 8)))
+        ;data-pricing-filtered (filter #(> (datecore/date-time (cljs.reader/read-string (subs (% :date) 0 4)) (cljs.reader/read-string (subs (% :date) 5 7)) (cljs.reader/read-string (subs (% :date) 7 9))) start-date-dateformat) data-pricing)
+        ;data-pricing-filtered (filter #(> (% :date) start-date-yyyy-mm-dd) data-pricing)
         ]
-
-    (println start-date-yyyy-mm-dd)
-    (println (:date (first data-pricing)))
-    ()
+    ;(println (datecore/date-time 2015 1 8))
     {:$schema "https://vega.github.io/schema/vega-lite/v4.json",
      :resolve {:scale {:color "independent"}}
      :title   nil
