0454dc8fe6905abd5e1c3d861b2ba60c03fe5cf0
fcadet <florian.kd@gmail.com>
2023-01-05 10:45:20 +0000

    adding 2022 to attribution history

--- src/cljs/jasminegui/attribution.cljs
+++ src/cljs/jasminegui/attribution.cljs
@@ -386,6 +386,7 @@
                           ;:monthly {:periodseq (conj (vec (repeat (count months-this-year) "monthly")) "mtd") :fileperiodseq (conj (vec (repeat (count months-this-year) "monthly_")) nil) :dateseq (conj months-this-year (js/parseInt (t/gdate->yyyyMMdd qt-date)))}
                           :monthly {:periodseq (conj (vec (repeat (count months-this-year) "monthly")) "mtd" "ytd") :fileperiodseq (conj (vec (repeat (count months-this-year) "monthly_")) nil nil) :dateseq (conj months-this-year (js/parseInt (t/gdate->yyyyMMdd qt-date)) (js/parseInt (t/gdate->yyyyMMdd qt-date)))}
                           :daily {:periodseq ["yearly" "yearly" "yearly" "yearly"] :fileperiodseq ["yearly_" "yearly_" "yearly_" nil] :dateseq [20201231 20211231 20221231 20220706]}))]
+
     [box :class "subbody rightelement" :child
      (gt/element-box-generic "attribution-history-risk-table" max-width (str "Portfolio history")
                              {:target-id "attribution-history-risk-table" :on-click-action #(tools/react-table-to-csv @attribution-history-display-view @portfolio download-columns is-tree)}
